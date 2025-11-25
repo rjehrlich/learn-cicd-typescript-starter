3a1719e9a5521d939d202cee6b8463f3959d9a23
rjsehrlich <racheljsehrlich@gmail.com>
2025-11-25 16:59:04 -0700

    commit broken code to test the ci

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -3,7 +3,7 @@ import { IncomingHttpHeaders } from "http";
 export function getAPIKey(headers: IncomingHttpHeaders): string | null {
   const authHeader = headers["authorization"];
   if (!authHeader) {
-    return null;
+    return "try and break test";
   }
 
   const splitAuth = authHeader.split(" ");
